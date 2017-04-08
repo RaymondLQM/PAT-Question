ece6df27003cc392b925388c3f7e4b8832f095c7
RaymondLQM <lqmYoung@Outlook.com>
2017-04-08 09:43:40 +0800

    test

---
+++ PAT-Base level/B-1001.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+	int num, count;
+	count = 0;
+	scanf("%d", &num);
+	while(num != 1){
+		if(num%2 == 0)num = num / 2;
+		else num = (3 * num + 1) / 2;
+		count++;
+	}
+	printf("%d", count);
+	return 0;
+}
